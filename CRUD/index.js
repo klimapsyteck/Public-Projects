a54b6e6ac3524e22202ce107a281b42d9067c6e6
Kesley Lima <kesleylimasi@gmail.com>
2022-12-14 21:19:28 -0300

    Create done

--- CRUD/index.js
+++ CRUD/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+const Person = require('./models/Person')
 
 app.use(
     express.urlencoded({
@@ -8,3 +11,34 @@ app.use(
 )
 
 app.use(express.json())
+
+app.post('/person', async (req, res) =>{
+    const {name, approved} = req.body
+    const person = {
+        name,
+        approved
+    }
+
+    try {
+        Person.create(person)
+
+        res.status(201).json('Created')
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
+app.get('/', (req, res) =>{
+    
+    res.json({message: "Working"})
+})
+const DB_USER = 'klimapsyteck'
+const DB_PASSWORD = 'jTMhRkC3CpAPHvVD'
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@crudapi.jae0sv6.mongodb.net/?retryWrites=true&w=majority`)
+    .then( () => {
+        console.log('Database Connected')
+        app.listen(3000)
+    })
+    .catch(err => console.log(err))
+
+
